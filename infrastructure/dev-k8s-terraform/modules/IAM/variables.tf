5b85fbfbd78ce94515264fd3f1884e78af37bdd5
andy <andy@clarusway.com>
2023-01-22 21:09:26 +0000

    changed role names for TF

--- infrastructure/dev-k8s-terraform/modules/IAM/variables.tf
+++ infrastructure/dev-k8s-terraform/modules/IAM/variables.tf
@@ -1,31 +1,31 @@
 variable "master-role-policy" {
-  default = "petclinic_policy_for_master_role"
+  default = "petclinic_policy_for_master_role_andy"
 }
 
 variable "worker-role-policy" {
-  default = "petclinic_policy_for_worker_role"
+  default = "petclinic_policy_for_worker_role_andy"
 }
 
 variable "master-role" {
-  default = "petclinic_role_master_k8s"
+  default = "petclinic_role_master_k8s_andy"
 }
 
 variable "worker-role" {
-  default = "petclinic_role_worker_k8s"
+  default = "petclinic_role_worker_k8s_andy"
 }
 
 variable "master-role-attachment" {
-  default = "petclinic_attachment_for_master"
+  default = "petclinic_attachment_for_master_andy"
 }
 
 variable "worker-role-attachment" {
-  default = "petclinic_attachment_for_worker"
+  default = "petclinic_attachment_for_worker_andy"
 }
 
 variable "profile_for_master" {
-  default = "petclinic_profile_for_master"
+  default = "petclinic_profile_for_master_andy"
 }
 
 variable "profile_for_worker" {
-  default = "petclinic_profile_for_worker"
+  default = "petclinic_profile_for_worker_andy"
 }
