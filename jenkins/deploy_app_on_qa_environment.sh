d0b37217ae38c653a4c1b6019610c604f521477e
andy <andy@clarusway.com>
2023-01-28 07:08:51 +0000

    updated

--- jenkins/deploy_app_on_qa_environment.sh
+++ jenkins/deploy_app_on_qa_environment.sh
@@ -4,7 +4,7 @@ sed -i s/HELM_VERSION/${BUILD_NUMBER}/ k8s/petclinic_chart/Chart.yaml
 AWS_REGION=$AWS_REGION helm repo add stable-petclinic s3://petclinic-helm-charts-james/stable/myapp/ || echo "repository name already exists"
 AWS_REGION=$AWS_REGION helm repo update
 helm package k8s/petclinic_chart
-AWS_REGION=$AWS_REGION helm s3 push --force petclinic_chart-0.0.1.tgz stable-petclinic
+AWS_REGION=$AWS_REGION helm s3 push --force petclinic_chart-${BUILD_NUMBER}.tgz stable-petclinic
 kubectl create ns petclinic-qa || echo "namespace petclinic-qa already exists"
 kubectl delete secret regcred -n petclinic-qa || echo "there is no regcred secret in petclinic-qa namespace"
 kubectl create secret generic regcred -n petclinic-qa \
@@ -12,5 +12,5 @@ kubectl create secret generic regcred -n petclinic-qa \
     --type=kubernetes.io/dockerconfigjson
 AWS_REGION=$AWS_REGION helm repo update
 AWS_REGION=$AWS_REGION helm upgrade --install \
-    petclinic-app-release stable-petclinic/petclinic_chart --version 0.0.1 \
+    petclinic-app-release stable-petclinic/petclinic_chart --version ${BUILD_NUMBER} \
     --namespace petclinic-qa
